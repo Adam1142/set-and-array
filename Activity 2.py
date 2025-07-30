668e8e85609d1ab19d2155d6e190770351a2206c
Adam1142 <doodoohsb@gmail.com>
2025-07-30 17:18:58 +0100

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,17 @@
+set1 = {1, 5, 9, 3, 4, 2}
+print(set1)
+
+set2 = {"a", "z", 1.34, 7, (4, 97, 63)}
+print(set2)
+
+set3 = {2, 5, 7, 2, 3, 7}
+print(set3)
+
+set4 = set([2, 4, 8, 5, 3, 9])
+print(set4)
+
+set5 = set([0, 1, 2, 3, 4, 5])
+print(set5)
+set5.pop(0)
+print("after removing the first element from the set:")
+print(set5)
